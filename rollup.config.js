6cd8fe39fb8c337efde7178edd901682c57950ec
Konsta Purtsi <konsta@identio.fi>
2020-11-22 00:18:58 +0200

    Add scss and minifying bundle into svelte config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import preprocess from 'svelte-preprocess';
+import { terser } from 'rollup-plugin-terser';
 
+const isProduction = true;
 export default {
   input: 'src/main.js',
   output: {
@@ -13,12 +16,14 @@ export default {
   plugins: [
     svelte({
       // enable run-time checks when not in production
-      dev: !process.env.ROLLUP_WATCH, 
+      dev: !isProduction, 
       // we'll extract any component CSS out into
 			// a separate file - better for performance
       css: css => {
         css.write('bundle.css');
-      }
+      },
+      // preprocess scss
+      preprocess: preprocess()
     }),
 
     // resolve external dependencies
@@ -27,5 +32,8 @@ export default {
       dedupe: ['svelte'],
     }),
     commonjs(),
+
+    // minify bundle for production
+    isProduction && terser()
   ]
 }
